84ac1c7e331e95b96eb02b0536607b026ba64d20
sofia151204 <sofiavl1504@gmail.com>
2025-10-02 07:57:38 -0500

    Mejoras generales en juego.cpp y main.cpp: validación, referencias y reinicio

--- main.cpp
+++ main.cpp
@@ -1,16 +1,1 @@
-#include "juego.h"
-#include <iostream>
-using namespace std;
-
-int main() {
-    string nombre1, nombre2;
-    cout << "Ingrese el nombre del Jugador 1 (X): ";
-    getline(cin, nombre1);
-    cout << "Ingrese el nombre del Jugador 2 (O): ";
-    getline(cin, nombre2);
-
-    juego partida(nombre1, nombre2);
-    partida.iniciarJuego();
-
-    return 0;
-}
+    
